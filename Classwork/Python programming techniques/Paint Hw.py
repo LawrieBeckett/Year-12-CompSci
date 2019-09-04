89632e0b92305db81187759445912a9ac7abb26f
LawrieBeckett <36962757+LawrieBeckett@users.noreply.github.com>
2019-09-04 13:15:27 +0100

    Create Paint Hw.py

---
+++ Classwork/Python programming techniques/Paint Hw.py
@@ -0,0 +1,24 @@
+units = int(input("1: Imperial, 2 Metric))
+
+if units == 1:
+    h = float(input("Please Enter the room's height in feet: "))
+    w = float(input("Please Enter the room's width in feet: "))
+    d= float(input("Please Enter the room's depth in feet: "))
+    sq = h * w * 2
+    sqa = h * d * 2
+    squareft = sq + sqa
+    print("You will need ", squareft , " square feet of paint for this room.")
+
+elif units == 2:
+    h = float(input("Please Enter the room's height in metres: "))
+    w = float(input("Please Enter the room's width in metres: "))
+    d= float(input("Please Enter the room's depth in metres: "))
+    sq = h * w * 2
+    sqa = h * d * 2
+    squareft = sq + sqa
+    print("You will need ", squareft , " square metres of paint for this room.")
+
+else:
+    print("invalid input")
+
+#program assumes room is cuboid
